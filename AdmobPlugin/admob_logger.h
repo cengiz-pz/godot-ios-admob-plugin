fb63ccd6b058c0c388b2d2603cd7bc78c4b40338
ZT Pawer <admin@ztpawer.com>
2025-04-11 23:41:11 -0500

    Use os_log instead on NSLog (#13)
    * implement a lighter version with no log on device console
    
    * remove compilation error for static text
    
    * add errors os_log_error
    
    * change namespace to generic
    
    * Implement suggestions
    
    * fix deinit compilation issue see https://stackoverflow.com/questions/61470469/how-to-release-os-log-t-types-returned-by-os-log-create-on-macos
    
    * re-instate addon

---
+++ AdmobPlugin/admob_logger.h
@@ -0,0 +1,10 @@
+// AdmobLogger.h
+#ifndef ADMOB_LOGGER_H
+#define ADMOB_LOGGER_H
+
+#include <os/log.h>
+
+// Declare the shared os_log_t instance
+extern os_log_t admob_log;
+
+#endif // ADMOB_LOGGER_H
